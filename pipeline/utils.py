b32af3e6e687bf0bf433c7ac6d0dccf2652503ba
Iasmin Gomes <iasmin.ufpe@gmail.com>
2025-12-01 06:19:47 -0300

    adiciona função de chamada da API da OpenAI e arquivo do verificador

---
+++ pipeline/utils.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def call_llm(prompt, model="gpt-4o-mini", max_tokens=2000):
+    response = client.chat.completions.create(
+        model=model,
+        # O parâmetro messages é uma lista com o histórico da conversa
+        # Como queremos recomeçar sempre da saída anterior gerada pela LLM, não mantemos o histórico
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=max_tokens,
+        temperature=0.1 # Controla o grau de criatividade. Devemos deixar rígido assim?
+    )
+    # O modelo retorna a(s) resposta(s) em uma lista
+    # O parâmetro n, que controla o número de respostas, é configurado com 1 por padrão
+    return response.choices[0].message.content
